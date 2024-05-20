a2045eecf6ceeadbe27fbbaa1ce42e3c604ae7b8
JudithMathabathe <112932531+JudithMathabathe@users.noreply.github.com>
2024-05-20 21:11:11 +0200

    created .net app for my apis, added sevices on my frontend

---
+++ web-apis/ContactsApi/Models/Contacts.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactsApi.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public long TelephoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+    }
+}

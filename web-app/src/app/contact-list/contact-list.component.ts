56fae4f675a7e33b466c6aade1100406cceb039c
JudithMathabathe <112932531+JudithMathabathe@users.noreply.github.com>
2024-05-21 05:35:55 +0200

    update contact form to use formGroup

--- web-app/src/app/contact-list/contact-list.component.ts
+++ web-app/src/app/contact-list/contact-list.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { ContactService } from '../contact.service';
+import { Contact } from '../models/contact.model';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [],
-  templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.css'
+  imports: [CommonModule],
+  providers: [HttpClient],
+  templateUrl: 'contact-list.component.html',
+  styleUrls: ['contact-list.component.css'],
 })
 export class ContactListComponent {
+  // Initializing empty contacts array
+  contacts: Contact[] = [];
 
+  // Injecting ContactService
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit() {
+    this.getContacts();
+  }
+
+  getContacts() {
+    this.contactService
+      .getContacts()
+      .subscribe((contacts) => (this.contacts = contacts));
+  }
+
+  add_contact(contact: Contact) {
+    this.contactService.addContact(contact);
+  }
+
+  delete_contact(id?: number) {
+    if (id) {
+      this.contactService.deleteContact(id);
+    } else {
+      console.error('Error: Cannot delete contact with undefined ID.');
+    }
+  }
+
+  edit_contact(contact: Contact){
+    this.contactService.updateContact(contact);
+  }
 }
--- web-app/src/app/contact.service.ts
+++ web-app/src/app/contact.service.ts
@@ -1,35 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Contact } from '../app/models/contact.model'
+import { Contact } from '../app/models/contact.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
-  private api_url: string;
+  private apiUrl = 'http://localhost:5159/api';
 
-  constructor(private http: HttpClient) {
-    this.api_url = process.env.API_URL;
+  constructor(private http: HttpClient) {}
+
+  getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>(`${this.apiUrl}/contacts/get_contacts`);
   }
 
-  // Method to get all contacts
-  get_contacts(): Observable<Contact[]> {
-    return this.http.get<Contact[]>(`${this.api_url}/contacts`);
+  addContact(contact: Contact): Observable<Contact> {
+    return this.http.post<Contact>(`${this.apiUrl}/contacts/create_contact`, contact);
   }
 
-  // Method to add a new contact
-  add_contact(contact: Contact): Observable<Contact> {
-    return this.http.post<Contact>(`${this.api_url}/contacts`, contact);
+  updateContact(contact: Contact): Observable<Contact> {
+    return this.http.put<Contact>(`${this.apiUrl}/contacts/update_contact/${contact.id}`, contact);
   }
 
-  // Method to update an existing contact
-  update_contact(contact: Contact): Observable<Contact> {
-    return this.http.put<Contact>(`${this.api_url}/contacts/${contact.id}`, contact);
+  deleteContact(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/contacts/delete_contact/${id}`);
   }
-
-  // Method to delete a contact by ID
-  delete_contact(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.api_url}/contacts/${id}`);
+  
+  editContact(contact: Contact): Observable<Contact> {
+    return this.http.put<Contact>(`${this.apiUrl}/contacts/update_contact/${contact.id}`, contact);
   }
 }
